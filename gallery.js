f082dfe77a35615af3005e502ea83968e1172d1b
Johann-Ezekiel-Rondilla <jebrondilla@mymail.mapua.edu.ph>
2025-03-08 08:04:02 +0000

    Edited code

--- gallery.js
+++ gallery.js
@@ -3,11 +3,13 @@ window.onload = initializeGallery;
 function initializeGallery() {
     console.log("Page loaded. Initializing gallery...");
     let images = document.querySelectorAll(".preview");
-    
+
     images.forEach((img) => {
-        img.setAttribute("tabindex", "0"); // Make images focusable
+        img.setAttribute("tabindex", "0");
         img.addEventListener("focus", function () { upDate(this); });
         img.addEventListener("blur", function () { unDo(); });
+        img.addEventListener("mouseover", function () { upDate(this); });
+        img.addEventListener("mouseout", function () { unDo(); });
     });
 }
 
